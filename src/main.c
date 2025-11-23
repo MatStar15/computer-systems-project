13b7f16fe58475db5f8b8d53b497938413c47cb9
torwanat <nawrotm37@gmail.com>
2025-11-23 14:36:17 +0200

    Refactor code and add  more comments

--- src/main.c
+++ src/main.c
@@ -16,36 +16,38 @@
 #define MESSAGE_BUFFER_LENGTH 255
 #define RX_COMMAND_BUFFER_LENGTH 64
 
-//Add here necessary states
+//Program states
 enum state { SENDING=1, READY_TO_SEND, READING, PROCESSING_MESSAGE, IN_MENU, PLAYING_SONG };
 enum state programState = READING;
 
+//Orientation states
 enum orientation { HORIZONTAL=1, VERTICAL};
 enum orientation orientationState = HORIZONTAL; 
 
 bool button1Pressed = false;
 bool button2Pressed = false;
 
+// Message buffer
 uint16_t messageCounter = 0;
 char messageBuffer[MESSAGE_BUFFER_LENGTH];
 
+// Menu options
 char menuOptions[5][10] = {"SELECT", "SONG 1", "SONG 2", "SONG 3", "EXIT"};
 uint8_t currentMenuOption = 0;
 
+// Menu songs (stolen from AI)
 int16_t song1[] = {
     330, 294, 262, 294, 330, 330, 330, 0,    /* E D C D E E E - pause */
     294, 294, 294, 330, 392, 392, 0,         /* D D D E G G - pause */
     330, 294, 262, 294, 330, 330, 330, 0,    /* E D C D E E E - pause */
     330, 294, 294, 330, 294, 262, -1         /* E D D E D C */
 };
-//happy birthday melody
 int16_t song2[] = {
     392, 392, 440, 392, 523, 494, 0,         /* Hap-py birth-day to you - pause */
     392, 392, 440, 392, 587, 523, 0,         /* Hap-py birth-day to you - pause */
     392, 392, 784, 659, 523, 494, 440, 0,    /* Hap-py birth-day dear ... - pause */
     698, 698, 659, 523, 587, 523 , -1        /* ... name - pause */
 };
-//twinkle twinkle little star melody
 int16_t song3[] = {
     330, 330, 330, 0,                        /* Jin-gle bells - pause */
     330, 330, 330, 0,                        /* Jin-gle bells - pause */
@@ -64,7 +66,7 @@ static void play_buzzer(char *message);
 static void translate_morse2alpha(char *morseMessage, char *alphaMessage, uint8_t messageLength);
 static void play_song(uint8_t songIndex);
 
-
+// IMU data reading task
 static void data_task(void *arg){
     (void)arg;
     vTaskDelay(pdMS_TO_TICKS(10000));
@@ -72,19 +74,18 @@ static void data_task(void *arg){
     ICM42670_start_with_default_values();
     // printf("ICM42670 initialized\n");
 
-
     float ax, ay, az, gx, gy, gz, t;
     bool first_run = true;
 
     for(;;){
-
+        // Do stuff only if in READING state
         if (programState != READING){
             vTaskDelay(pdMS_TO_TICKS(200));
             continue;
         }
 
 
-        tight_loop_contents(); // Modify with application code here.
+        tight_loop_contents();
 
         ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t);
 
@@ -96,6 +97,7 @@ static void data_task(void *arg){
             orientationState = VERTICAL;
         }
 
+        // Set initial orientation on first run
         if (first_run){
             orientationState = HORIZONTAL;
             first_run = false;
@@ -107,11 +109,12 @@ static void data_task(void *arg){
     }
 }
 
+//Task for sending message via USB
 static void usb_sending_task(void *arg){
     (void)arg;
 
     for(;;){
-        tight_loop_contents(); // Modify with application code here.
+        tight_loop_contents(); 
 
         // Do stuff only if a message is ready to send
         if(programState == READY_TO_SEND){
@@ -121,15 +124,13 @@ static void usb_sending_task(void *arg){
             messageBuffer[messageCounter] = '\0';
             printf("Morse message: %s\n", messageBuffer);
 
+            // Translate morse to alphabet and send
             char alphaMessage[MESSAGE_BUFFER_LENGTH];
             translate_morse2alpha(messageBuffer, alphaMessage, messageCounter);
             printf("Translated message: %s\n", alphaMessage);
 
             // Indicate message sent by blinking LED 3 times
             blink_led(3);
-
-
-
 
             // Reset for the next message
             messageCounter = 0;
@@ -143,11 +144,11 @@ static void usb_sending_task(void *arg){
     }
 }
 
-
+// Task for receiving commands and messages via USB
 static void usb_receiving_task(void *arg){
     (void)arg;
 
-    // Buffer to store incoming commands
+    // Buffer to store incoming commands and messages
     char rxBuffer[RX_COMMAND_BUFFER_LENGTH];
     uint16_t rxCounter = 0;
 
@@ -155,28 +156,32 @@ static void usb_receiving_task(void *arg){
         int c = getchar();
 
         if (c != EOF) {
-            
+            // Check for end of command/message
             if (c == '\n' || c == '\r') {
                 rxBuffer[rxCounter] = '\0';
 
+                // Process the received command/message
                 if (rxCounter > 0 && programState == READING) {
 
                     programState = PROCESSING_MESSAGE;
                     
-                    // Plan: "check /led" -> "to LED"
                     if (strcmp(rxBuffer, "/led") == 0) {
+                        // Test LED by blinking 5 times
                         blink_led(5);
                         programState = READING;
                     }
                     else if (strcmp(rxBuffer, "/buzzer") == 0) {
+                        // Test buzzer by playing a tone
                         buzzer_play_tone(500, 200);
                         programState = READING;
                     } 
                     else if (strcmp((rxBuffer), "/clear") == 0) {
+                        // Clear the display
                         clear_display();    
                         programState = READING;
                     }
                     else if (strcmp((rxBuffer), "/play") == 0) {
+                        // Enter the menu to select a song
                         currentMenuOption = 0;
                         programState = IN_MENU;
                     }
@@ -184,11 +189,13 @@ static void usb_receiving_task(void *arg){
                         // Write message to lcd screen
                         clear_display();
 
+                        // Translate morse to alphabet and write to the display
                         char alphaMessage[MESSAGE_BUFFER_LENGTH];
                         translate_morse2alpha(rxBuffer, alphaMessage, rxCounter);
                         // printf("Translated message: %s\n", alphaMessage);
                         write_text(alphaMessage);
 
+                        // Play the morse code message via buzzer
                         play_buzzer(rxBuffer);
 
                         programState = READING;
@@ -214,18 +221,21 @@ static void usb_receiving_task(void *arg){
     }
 }
 
+// Task for handling menu navigation and selection
 static void menu_task(void *arg){
     (void)arg;
     uint8_t selectedMenuOption = -1;
 
     for(;;){
-        tight_loop_contents(); // Modify with application code here.
+        tight_loop_contents();
 
+        // Do stuff only if in IN_MENU state
         if(programState != IN_MENU){
             vTaskDelay(pdMS_TO_TICKS(400));
             continue;
         }
 
+        // Update display if menu option has changed
         if(currentMenuOption != selectedMenuOption){
             selectedMenuOption = currentMenuOption;
             clear_display();
@@ -236,16 +246,20 @@ static void menu_task(void *arg){
     }
 }
 
+// Task for handling button presses
 static void button_task(void *arg){
     (void)arg;
     static uint8_t spaceCounter = 0;
 
     for(;;){
-        tight_loop_contents(); // Modify with application code here.
+        tight_loop_contents();
 
+        // Handle button presses based on current program state
         if (programState == READING){
 
+            // Handle button presses for morse code input
             if(button1Pressed){
+                // Add dot or dash based on orientation
                 if(orientationState == HORIZONTAL){
                     messageBuffer[messageCounter] = '-';
                     messageCounter++;
@@ -262,6 +276,7 @@ static void button_task(void *arg){
             }
 
             if(button2Pressed){
+                // Add space
                 messageBuffer[messageCounter] = ' ';
                 messageCounter++;
                 spaceCounter++;
@@ -276,6 +291,7 @@ static void button_task(void *arg){
                 // printf("%c`", messageBuffer[messageCounter-1]);
             }
         } else if (programState == IN_MENU){
+            // Handle button presses for menu navigation
 
             if(button1Pressed){
                 // Move to next menu option
@@ -291,11 +307,14 @@ static void button_task(void *arg){
                     programState = READING;
                 } else if (strcmp(menuOptions[currentMenuOption], "SELECT") != 0){
                     // printf("Playing %s\n", menuOptions[currentMenuOption]);
+
                     clear_display();
                     write_text("PLAYING");
+                    
                     // Play song based on selection
-                    // (Implementation of song playing not shown here)
                     play_song(currentMenuOption);
+
+                    // After playing, return to menu
                     clear_display();
                     write_text(menuOptions[currentMenuOption]);
                 }
@@ -310,6 +329,7 @@ static void button_task(void *arg){
     }
 }
 
+// GPIO interrupt callback for button presses
 static void gpio_callback(uint gpio, uint32_t events) {
     // printf("GPIO Interrupt on pin %d, events: %u\n", gpio, events);
     if (gpio == BUTTON1  && (events ==  8)) {
@@ -329,6 +349,7 @@ static void translate_morse2alpha(char *morseMessage, char *alphaMessage, uint8_
     // printf("Translating morse message: %s\n", morseMessage);
     // printf("Message length: %d\n", messageLength);
 
+    // Morse code tree traversal
     uint8_t counter = 0;
     uint8_t index = 1;
     for (size_t i = 0; i < messageLength; i++){
@@ -345,30 +366,36 @@ static void translate_morse2alpha(char *morseMessage, char *alphaMessage, uint8_
 	    }
     }
 
-
+    // Add last letter and null-terminate the string
     alphaMessage[counter] = letter[index];
     alphaMessage[counter + 1] = '\0';
 }
 
+// Play buzzer based on morse code string
 static void play_buzzer(char *string){
     
     char *ptr;
 
     for (ptr = string; *ptr != 0; ptr++) {
         if (*ptr == '-') {
+            // Dash
             buzzer_play_tone(440, 300);
         } else if (*ptr == '.') {
+            // Dot
             buzzer_play_tone(440, 100);
         } else {
+            // Space
             vTaskDelay(pdMS_TO_TICKS(500));                    
         }
         vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
+// Play song based on menu selection
 static void play_song(uint8_t songIndex){
     programState = PLAYING_SONG;
 
+    // Validate song index
     if (songIndex < 1 || songIndex > sizeof(songs) / sizeof(songs[0])){
         programState = IN_MENU;
         return;
@@ -376,6 +403,7 @@ static void play_song(uint8_t songIndex){
 
     // printf("Playing song %d\n", songIndex) - 1;
 
+    // Play notes until the ending note (-1) is reached
     for (size_t i = 0; songs[songIndex - 1][i + 1] >= 0; i++){
         int16_t frequency = songs[songIndex - 1][i];
         // printf("Playing frequency: %d\n", frequency);
